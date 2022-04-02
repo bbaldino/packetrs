a7b63f328d9673eb0d2426e893b137b9f4add0dd
bbaldino <bbaldino@cisco.com>
2022-04-02 14:20:03 -0700

    define macro attribute

--- macro/src/lib.rs
+++ macro/src/lib.rs
@@ -5,7 +5,7 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 use syn::parse_macro_input;
 
-#[proc_macro_derive(Packetrs)]
+#[proc_macro_derive(Packetrs, attributes(packetrs))]
 /// Document your macro here.
 pub fn derive_packetrs(item: TokenStream) -> TokenStream {
     let item = parse_macro_input!(item as proc_macro2::TokenStream);
